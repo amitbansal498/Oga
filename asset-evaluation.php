28e10fa7d7d329b810fcf2feb69af94fa6ccdda6
Amit Bansal <amitbansal@oges.info>
2015-11-06 20:00:00 -0600

    change in file

---
+++ asset-evaluation.php
@@ -0,0 +1,222 @@
+<?php include_once('api/jsondata.php'); ?>
+<!DOCTYPE html>
+<html>
+<!-- Mirrored from abcgomel.ru/haswell-1.6-demo/index2.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 25 Jul 2016 09:35:22 GMT -->
+<head>
+		<title>ASSET EVALUATION</title>
+		<meta charset=utf-8 >
+		<!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge"><![endif]-->
+		<meta name="robots" content="index, follow" > 
+		<!-- FAVICONS -->
+    <link rel="shortcut icon" href="images/favicon/favicon.png">
+<!-- CSS -->
+    <!-- REVOSLIDER CSS SETTINGS -->
+    <link rel="stylesheet" type="text/css" href="rs-plugin/css/settings.min.css" media="screen" />
+    <!--  BOOTSTRAP -->
+		<link rel="stylesheet" href="css/bootstrap.min.css"> 
+    <!--  GOOGLE FONT -->		
+		<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700%7COpen+Sans:400,300,700' rel='stylesheet' type='text/css'>
+    <!-- ICONS ELEGANT FONT & FONT AWESOME & LINEA ICONS  -->		
+		<link rel="stylesheet" href="css/icons-fonts.css" >	
+    <!--  CSS THEME -->		
+		<link rel="stylesheet" href="css/style.css" >
+    <!-- ANIMATE -->	
+		<link rel='stylesheet' href="css/animate.min.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+.owl-item { height:120px !important; border: 1px solid #eee; width:150px; max-width:200px; }
+</style>
+		</head>
+	<body>
+		<!-- LOADER -->	
+		<div id="loader-overflow">
+      <div id="loader3">Please enable JS</div>
+    </div>	
+		<div id="wrap" class="boxed ">
+			<div class="grey-bg"> <!-- Grey BG  -->	
+				<?php include_once('header.php');?>
+        <!-- PAGE TITLE SMALL -->
+        <div class="page-title-cont page-title-small grey-dark-bg">
+          <div class="relative container align-left">
+            <div class="row">
+              <div class="col-md-12">
+                <h1 class="page-title">ADVISORY SERVICES - ASSET EVALUATION</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- FEATURES 7 -->
+        <div class="page-section pb-10">
+          <div class="fes7-img-cont col-md-5">
+            <div class="fes7-img" style="background-image: url(images/asset-evalution-big.png)"></div>
+          </div>
+          <div class="container">
+            <div class="row">
+              <div class="col-md-6 col-md-offset-6 fes7-text-cont p-80-cont">
+                <h1><span class="font-light">Turn your Discoveries into Executed Projects covering the entire well lifecycle
+                 </span></h1>
+                <p class="mb-60">
+             We combine volumetric and performance evaluation results to determine the exploration, exploitation,
+			 and remaining potential reserves and resources of all properties based on a quick and 
+			 reliable processing and analysis of various types of data with the use of best in class software.
+              </p>
+                <div class="row">
+                  <div class="col-md-6 col-sm-6">
+                    <div class="fes7-box wow fadeIn" >
+                      <div class="fes7-box-icon">
+                        <div class="icon icon-basic-clock"></div>
+                      </div>
+                      <h3><?php echo  strtoupper('Wide Variety');?> </h3>
+                      <p>
+					Estimation of proven, probable, and potential reserves
+					  </p>
+                    </div>
+                  </div>
+                  <div class="col-md-6 col-sm-6">
+                    <div class="fes7-box wow fadeIn" data-wow-delay="200ms">
+                      <div class="fes7-box-icon">
+                        <div class="icon icon-basic-magnifier-plus"></div>
+                      </div>
+                      <h3><?php echo  strtoupper('Complete Information ');?>  	</h3>
+                      <p>
+					Preparation of complete development plans
+					  </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-6 col-sm-6">
+                    <div class="fes7-box wow fadeIn" data-wow-delay="400ms">
+                      <div class="fes7-box-icon">
+                        <div class="icon icon-arrows-compress"></div>
+                      </div>
+                      <h3><?php echo  strtoupper('Designing');?>   </h3>
+                      <p>
+					 Planning and design of well completion and workover projects
+					  </p>
+                    </div>
+                  </div>
+                  <div class="col-md-6 col-sm-6">
+                    <div class="fes7-box wow fadeIn"  data-wow-delay="600ms">
+                      <div class="fes7-box-icon">
+                        <div class="icon icon-arrows-expand"></div>
+                      </div>
+                      <h3><?php echo  strtoupper('Holistic Approach');?>   </h3>
+                      <p>
+					 Designing the most effective processes driven by emerging new technologies
+                     </p>
+                    </div>
+                  </div>
+                </div> 
+              </div>
+            </div><!--end of row-->
+          </div>
+        </div>        
+				<!-- DIVIDER -->
+<hr class="mt-0 mb-0">	
+<!-- FEATURES 4 -->
+<div class="page-section fes2-cont">
+<div class="container">
+<div class="row">
+<div class="col-xs-12 col-sm-4 col-md-4">
+<div class="fes4-box wow fadeIn">
+<h2 class="section-title">Benefits From 
+<span class="bold"> Asset Evaluation</span></h2>
+</div>
+</div>            
+<div class="col-xs-12 col-sm-4 col-md-8">
+<div class="fes4-box wow fadeIn" data-wow-delay="200ms">
+<div class="pt-15">
+The invaluable and vast experience of our team, 
+comprising of subject matter experts and senior professionals, allows us to deliver a robust and accountable 
+valuation of your oil & gas assets by adopting the most appropriate valuation methodology.
+</div>
+</div>
+</div>
+</div>
+<div class="row">
+<div class="col-xs-12 col-sm-4 col-md-4">
+<div class="fes4-box wow fadeIn" data-wow-delay="600ms">
+<div class="fes4-title-cont" >
+<div class="fes4-box-icon">
+<div class="icon icon-basic-gear"></div>
+</div>
+<h3><span class="bold"><?php echo  strtoupper('Experienced Consultants');?></span></h3>
+</div>
+<div class="pt-20">
+Get accurate technical/economic evaluation through experts
+</div>
+</div>
+</div>
+<div class="col-xs-12 col-sm-4 col-md-4">
+<div class="fes4-box wow fadeIn" data-wow-delay="800ms" >
+<div class="fes4-title-cont" >
+<div class="fes4-box-icon">
+<div class="icon icon-basic-geolocalize-05"></div>
+</div>
+<h3><span class="bold"><?php echo  strtoupper('Quick Results');?></span></h3>
+</div>
+<div class="pt-20">
+High-level quick turnaround review and opinion
+</div>
+</div>
+</div>
+<div class="col-xs-12 col-sm-4 col-md-4">
+<div class="fes4-box wow fadeIn" data-wow-delay="1000ms">
+<div class="fes4-title-cont" >
+<div class="fes4-box-icon">
+<div class="fa fa-user-circle"></div>
+</div>
+<h3><span class="bold"><?php echo  strtoupper('Range Determination ');?> </span></h3>
+</div>
+<div class="pt-20">
+Determination of bid range and ROI of an asset
+</div>
+</div>
+</div>              
+</div>
+</div>
+</div>
+
+ <!-- CASE STUDY -->
+
+<hr class="mt-0 mb-0">	
+<!-- OUR CLENTS -->
+<div class="page-section">
+<div class="container fes2-cont">
+<div class="mb-50">
+<h2 class="section-title">OUR  <span class="bold">CLIENTS</span></h2>
+</div>
+<div id="carousels" class="bs-docs-section">
+<div class="container mb-30" >
+<div class="owl-clients-nav owl-carousel owl-arrows-bg pt-4 pb-4">
+<div class="item m-bot-0 text-center"><img src="images/clients/12.png" alt="client" class="height-120"></div>
+<div class="item m-bot-0 text-center"><img src="images/clients/9.png" alt="client" class="height-120"></div>
+<div class="item m-bot-0 text-center"><img src="images/clients/6.png" alt="client" class="height-120"></div>
+<div class="item m-bot-0 text-center"><img src="images/clients/14.png" alt="client" class="height-120"></div>
+<div class="item m-bot-0 text-center"><img src="images/clients/15.png" alt="client" class="height-120"></div>
+</div>
+</div>
+</div>	
+</div>
+</div>
+<hr class="mt-0 mb-0">	
+<div class="container fes2-cont">
+<div class="col-md-12">
+<div class="mb-50">
+<h2 class="section-title">HOW TO <span class="bold">PURCHASE</span></h2>
+<h4> <p style="margin-left:40px;"><strong>Mail To : </strong><a href="mailto:sales@oges.info">sales@oges.info</a>,
+<a href="mailto: support@oges.info">&nbsp support@oges.info</p> </h4>
+</div>
+</div>
+<div class="text-right">
+<a href="advisory-services.php" class="button small gray-light blog-more text-right">BACK TO ADVISORY SERVICES</a>
+</div>
+</div>	 
+<!-- FOOTER 1 -->
+<?php include('footer.php');?>
+<?php echo POPUP_CASESTUDY();?>
+</div><!-- End BG -->	
+</div><!-- End wrap -->
+</body>
+</html>		
